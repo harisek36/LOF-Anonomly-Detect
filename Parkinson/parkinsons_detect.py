ad580d7ad55f6af418d711a4d2a75d05325eb7b7
gramsbyDill <dylanzslack@gmail.com>
2017-05-09 20:16:27 -0400

    remove unnecessary stuff

--- Parkinson/parkinsons_detect.py
+++
@@ -1,7 +0,0 @@
-import arff
-import numpy as np
-from lof_fast import lof
-
-a_data = arff.load(open('Parkinson/Parkinson_withoutdupl_05_v01.arff', 'rb'))
-
-"""Hope to compute anomolies across parkinson data set."""
